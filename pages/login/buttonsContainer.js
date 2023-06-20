84ac2a1c61cef6dfab597e45caef57b931bc4ece
César Henrique <cesaarhvb@gmail.com>
2023-06-20 12:38:30 -0300

    feat: implement container button

---
+++ pages/login/buttonsContainer.js
@@ -0,0 +1,7 @@
+function createButtonsContainer() {
+  const buttonsContainer = document.createElement('div');
+  buttonsContainer.setAttribute('id', 'buttonsContainer');
+  return buttonsContainer;
+}
+
+export default createButtonsContainer;

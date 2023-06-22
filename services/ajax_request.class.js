56dfa384fae86dc55f59fec7f95820f00d340945
cesarhenrq <cesaarhvb@gmail.com>
2023-06-22 04:09:10 -0300

    feat: add create, delete, edit and get task

--- services/ajax_request.class.js
+++ services/ajax_request.class.js
@@ -36,6 +36,100 @@ class AjaxRequest {
       });
     });
   }
+
+  static addTask(url, data) {
+    const requestUrl = "https://employ-back-end-1fa428ae58ff.herokuapp.com/";
+
+    const token = localStorage.getItem("token");
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: requestUrl + url,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        success: function (response) {
+          resolve(response);
+        },
+        error: function (xhr, status, error) {
+          reject(error);
+        },
+      });
+    });
+  }
+
+  static getTasks(url) {
+    const requestUrl = "https://employ-back-end-1fa428ae58ff.herokuapp.com/";
+
+    const token = localStorage.getItem("token");
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: requestUrl + url,
+        type: "GET",
+        contentType: "application/json",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+        success: function (response) {
+          resolve(response);
+        },
+        error: function (xhr, status, error) {
+          reject(error);
+        },
+      });
+    });
+  }
+
+  static deleteTask(url) {
+    const requestUrl = "https://employ-back-end-1fa428ae58ff.herokuapp.com/";
+
+    const token = localStorage.getItem("token");
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: requestUrl + url,
+        type: "POST",
+        contentType: "application/json",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+        success: function (response) {
+          resolve(response);
+        },
+        error: function (xhr, status, error) {
+          reject(error);
+        },
+      });
+    });
+  }
+
+  static editTask(url, data) {
+    const requestUrl = "https://employ-back-end-1fa428ae58ff.herokuapp.com/";
+
+    const token = localStorage.getItem("token");
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: requestUrl + url,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+        success: function (response) {
+          resolve(response);
+        },
+        error: function (xhr, status, error) {
+          reject(error);
+        },
+      });
+    });
+  }
 }
 
 export default AjaxRequest;

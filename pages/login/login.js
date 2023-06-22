5a00b76711210365971f8de76adf47da6beaa983
cesarhenrq <cesaarhvb@gmail.com>
2023-06-22 19:59:28 -0300

    refactor: cleaned the code

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,20 +3,22 @@ import AjaxRequest from "../../services/ajax_request.class.js";
 function login(event) {
   event.preventDefault();
 
-  $("#loginButton").attr("disabled", true);
+  const loginButton = $("#loginButton");
+  loginButton.prop("disabled", true);
+
   const email = $("#email").val();
   const password = $("#password").val();
+
+  if (!email || !password) {
+    loginButton.prop("disabled", false);
+
+    return alert("Please fill all the fields!");
+  }
 
   const formData = {
     email,
     password,
   };
-
-  if (!email || !password) {
-    $("#loginButton").attr("disabled", false);
-
-    return alert("Please fill all the fields!");
-  }
 
   AjaxRequest.login("users/login", formData)
     .then(function (response) {
@@ -30,9 +32,10 @@ function login(event) {
     })
     .catch(function (error) {
       alert(error);
+    })
+    .finally(function () {
+      loginButton.prop("disabled", false);
     });
-
-  $("#loginButton").attr("disabled", false);
 }
 
 export default login;

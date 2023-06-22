983e03cc81963227d983f7bd1c6c4498db425e6f
cesarhenrq <cesaarhvb@gmail.com>
2023-06-22 04:05:09 -0300

    feat: implement layout

---
+++ pages/task-manager/layout.js
@@ -0,0 +1,42 @@
+import "../../styles/task-manager/styles.css";
+import createAddTaskButton from "./addTaskButton";
+
+import createHeader from "./header";
+import createTable from "./table";
+
+import getTasks from "./getTasks";
+import renderTasks from "./renderTasks";
+
+const expires_in = JSON.parse(localStorage.getItem("data")).expires_in;
+
+setTimeout(() => {
+  localStorage.clear();
+  alert("Session expired. Please login again.");
+  window.location.href = "/";
+}, expires_in);
+
+$("#app").html(`
+  <div id="task-manager">
+    <div id="container">
+    </div>
+  </div>
+`);
+
+createHeader();
+
+createAddTaskButton();
+
+createTable();
+
+$("#table").ready(() => {
+  getTasks()
+    .then(function (tasks) {
+      console.log(tasks);
+      if (tasks.data !== "No tasks found for this user!") {
+        renderTasks(tasks.data);
+      }
+    })
+    .catch(function (error) {
+      alert(error);
+    });
+});
